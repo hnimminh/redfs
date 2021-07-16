c7d8bbd9d1ab09a1a2967cfd3605ddd91775f3a5
Minh Minh <hnimminh@outlook.com>
2021-07-16 15:15:25 +0700

    update usage

--- examples/asyncctl.py
+++ examples/asyncctl.py
@@ -1,26 +1,38 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
-
-import logging
 import gevent
 import redfs
+import traceback
 
 
-def on_sofia_register_failure(event):
-    message = 'Failed register attempt from {network-ip} to user {to-user} profile {profile-name}'
-    print(message.format(**event.headers))
+ESL_HOST = '127.0.0.1'
+ESL_PORT = 8021
+ESL_PASSWORD = 'your-esl-password'
+ESL_TIMEOUT = 10
 
-fs = redfs.InboundESL(host='192.168.50.4', port=8021, password='ClueCon')
-fs.connect()
-fs.register_handle('sofia::register_failure', on_sofia_register_failure)
-fs.send('EVENTS PLAIN ALL')
 
-print('Connected to FreeSWITCH!')
+def dosome(event):
+    event_name = event.headers.get('Event-Name')
+    uuid = event.headers.get('Unique-ID')
+    print(f'Event {event_name} with uuid {uuid}')
+
+    conn = redfs.InboundESL(host=ESL_HOST, port=ESL_PORT, password=ESL_PASSWORD, timeout=ESL_TIMEOUT)
+    conn.connect()
+
+    if event_name == 'CHANNEL_PARK':
+        cmd = f'api uuid_answer {uuid}'
+        res = conn.send(cmd)
+        print(cmd, res.data)
+
+
+cnx = redfs.InboundESL(host=ESL_HOST, port=ESL_PORT, password=ESL_PASSWORD, timeout=ESL_TIMEOUT)
+cnx.connect()
+cnx.register_handle('*', dosome)
+cnx.send('EVENTS PLAIN ALL')
+print('connected')
 while True:
     try:
         gevent.sleep(1)
     except KeyboardInterrupt:
-        fs.stop()
+        cnx.stop()
         break
-print('ESL Disconnected.')
+    except Exception as e:
+        print(e, traceback.format_exc())
